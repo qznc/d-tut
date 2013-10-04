a7d320fca65a60029c03454c5e071d188c8261c9
Andreas Zwinkau <qznc@web.de>
2013-10-04 14:03:55 +0200

    change theme

--- source/conf.py
+++ source/conf.py
@@ -92,12 +92,17 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = 'agogo'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+  "pagewidth":     "50em",
+  "documentwidth": "30em",
+  "sidebarwidth":  "20em",
+  "textalign":     "left",
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
