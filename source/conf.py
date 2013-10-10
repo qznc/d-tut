9075232a49b88f562a7cd27f19fc5302f82a3f04
Andreas Zwinkau <qznc@web.de>
2013-10-10 12:20:18 +0200

    Create my own theme: marcelene

--- source/conf.py
+++ source/conf.py
@@ -82,7 +82,7 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+#pygments_style = 'sphinx'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
@@ -92,7 +92,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'agogo'
+html_theme = 'marcelene'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -101,18 +101,17 @@ html_theme_options = {
   "pagewidth":     "55em",
   "documentwidth": "35em",
   "sidebarwidth":  "20em",
-  "textalign":     "left",
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Pragmatic D Tutorial"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+#html_short_title = "d-tut"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
