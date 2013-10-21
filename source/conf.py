87077b08c8c5858fbea5d2ff44fdaace5de3ece3
Andreas Zwinkau <zwinkau@kit.edu>
2013-10-21 10:09:42 +0200

    disable index

--- source/conf.py
+++ source/conf.py
@@ -143,10 +143,10 @@ html_last_updated_fmt = '%Y-%m-%d'
 #html_additional_pages = {}
 
 # If false, no module index is generated.
-#html_domain_indices = True
+html_domain_indices = False
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
