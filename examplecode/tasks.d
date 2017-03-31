2d91eb1272abb246f8fbfd092e86b13aa70d255e
woozie164 <woozie663@gmail.com>
2017-03-31 20:18:09 +0200

    Fixed compilation error
    This example doesn't compile anymore in more recent versions of dmd (I'm using v2.073.2). Moving the getTerm function outside main makes it work. 
    
    Relevant thread: http://forum.dlang.org/post/fhmyuhtwqwznoyahlxva@forum.dlang.org

--- examplecode/tasks.d
+++ examplecode/tasks.d
@@ -1,5 +1,14 @@
 import std.algorithm, std.parallelism, std.range;
 
+immutable n = 1_000_000_000;
+immutable delta = 1.0 / n;
+
+real getTerm(int i)
+{
+	immutable x = ( i - 0.5 ) * delta;
+	return delta / ( 1.0 + x * x ) ;
+}
+
 void main() {
     // Parallel reduce can be combined with
     // std.algorithm.map to interesting effect.
@@ -14,17 +23,8 @@ void main() {
     // TaskPool.reduce:       12.170 s
     // std.algorithm.reduce:  24.065 s
 
-    immutable n = 1_000_000_000;
-    immutable delta = 1.0 / n;
-
-    real getTerm(int i)
-    {
-        immutable x = ( i - 0.5 ) * delta;
-        return delta / ( 1.0 + x * x ) ;
-    }
-
     immutable pi = 4.0 * taskPool.reduce!"a + b"(
         std.algorithm.map!getTerm(iota(n))
     );
 }
-
+ 
